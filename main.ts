6aafbdb1b9532797fef61df492ba60e667b5d65f
alytas <156123368+alytas@users.noreply.github.com>
2024-02-26 11:14:12 -0600

    added lighting variables

--- main.ts
+++ main.ts
@@ -2,11 +2,163 @@ namespace SpriteKind {
     export const util = SpriteKind.create()
     export const npc = SpriteKind.create()
 }
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    characterAnimations.setCharacterState(mySprite, characterAnimations.rule(Predicate.FacingUp, Predicate.MovingUp))
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . . f f f f 2 2 f f f f . . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f f 2 f e f . . 
+        . . f f f 2 f e e 2 2 f f f . . 
+        . . f e 2 f f e e 2 f e e f . . 
+        . f f e f f e e e f e e e f f . 
+        . f f e e e e e e e e e e f f . 
+        . . . f e e e e e e e e f . . . 
+        . . . e f f f f f f f f 4 e . . 
+        . . . 4 f 2 2 2 2 2 e d d 4 . . 
+        . . . e f f f f f f e e 4 . . . 
+        . . . . f f f . . . . . . . . . 
+        `,img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . . f f f f 2 2 f f f f . . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e f 2 f f f 2 f 2 e f . . 
+        . . f f f 2 2 e e f 2 f f f . . 
+        . . f e e f 2 e e f f 2 e f . . 
+        . f f e e e f e e e f f e f f . 
+        . f f e e e e e e e e e e f f . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f e . . . 
+        . . 4 d d e 2 2 2 2 2 f 4 . . . 
+        . . . 4 e e f f f f f f e . . . 
+        . . . . . . . . . f f f . . . . 
+        `],
+    200,
+    characterAnimations.rule(Predicate.MovingUp, Predicate.FacingUp)
+    )
+    if (lighting == 1) {
+        flashlightangle = 1
+        flashlight.direction = -90
+    }
+})
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    flashlight.direction += -45
+	
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    flashlight.direction += 45
+	
+})
+function decision_maker (bool: boolean, num: number) {
+    controller.moveSprite(mySprite, 0, 0)
+    story.startCutscene(function () {
+        if (bool == true) {
+            story.cancelSpriteMovement(mySprite)
+            if (num == 1) {
+                story.printCharacterText("are you scared of the dark?")
+                story.showPlayerChoices("yes", "no")
+                if (story.getLastAnswer() == "no") {
+                    multilights.toggleLighting(false)
+                    controller.moveSprite(mySprite, 100, 100)
+                    lighting = 0
+                } else if (story.getLastAnswer() == "yes") {
+                    multilights.toggleLighting(true)
+                    story.printCharacterText("face your fears")
+                    tiles.placeOnTile(mySprite, tiles.getTileLocation(7, 24))
+                    controller.moveSprite(mySprite, 100, 100)
+                    lighting = 1
+                }
+            } else if (num == 0) {
+                story.printCharacterText("hello")
+                controller.moveSprite(mySprite, 100, 100)
+            }
+        } else {
+            controller.moveSprite(mySprite, 100, 100)
+        }
+    })
+}
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    characterAnimations.setCharacterState(mySprite, characterAnimations.rule(Predicate.FacingDown, Predicate.NotMoving))
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 4 4 5 5 4 4 f d 4 . . 
+        . . 4 4 . f f f f f f . 4 4 . . 
+        . . . . . f f . . f f . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.NotMoving, Predicate.FacingDown)
+    )
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     characterAnimations.setCharacterState(mySprite, characterAnimations.rule(Predicate.MovingLeft, Predicate.FacingLeft))
@@ -84,6 +236,10 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     200,
     characterAnimations.rule(Predicate.MovingLeft, Predicate.FacingLeft)
     )
+    if (lighting == 1) {
+        flashlightangle = 1
+        flashlight.direction = 180
+    }
 })
 controller.right.onEvent(ControllerButtonEvent.Released, function () {
     characterAnimations.setCharacterState(mySprite, characterAnimations.rule(Predicate.NotMoving))
@@ -247,38 +403,152 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     200,
     characterAnimations.rule(Predicate.MovingRight)
     )
+    if (lighting == 1) {
+        flashlightangle = 1
+        flashlight.direction = 0
+    }
 })
-function decision_maker (bool: boolean, num: number) {
-    controller.moveSprite(mySprite, 0, 0)
-    story.startCutscene(function () {
-        if (bool == true) {
-            story.cancelSpriteMovement(mySprite)
-            story.showPlayerChoices("a", "b")
-            if (story.getLastAnswer() == "a") {
-                multilights.toggleLighting(false)
-            } else if (story.getLastAnswer() == "b") {
-                multilights.toggleLighting(true)
-            }
-        } else {
-            tiles.placeOnTile(mySprite, tiles.getTileLocation(7, 24))
-        }
-    })
-}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile5`, function (sprite, location) {
     decision_maker(game.ask("touch the blue thing?"), randint(0, 1))
     tiles.placeOnTile(mySprite, tiles.getTileLocation(6, 4))
 })
+controller.up.onEvent(ControllerButtonEvent.Released, function () {
+    characterAnimations.setCharacterState(mySprite, characterAnimations.rule(Predicate.FacingUp, Predicate.NotMoving))
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 4 4 4 4 4 4 f d 4 . . 
+        . . 4 4 . f f f f f f . 4 4 . . 
+        . . . . . f f . . f f . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.NotMoving, Predicate.FacingUp)
+    )
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    characterAnimations.setCharacterState(mySprite, characterAnimations.rule(Predicate.FacingDown, Predicate.MovingDown))
+    characterAnimations.loopFrames(
+    mySprite,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . . f e 4 1 f d d f 1 4 e f . . 
+        . . . f e 4 d d d d 4 e f e . . 
+        . . f e f 2 2 2 2 e d d 4 e . . 
+        . . e 4 f 2 2 2 2 e d d e . . . 
+        . . . . f 4 4 5 5 f e e . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f . . . . . . . . . 
+        `,img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f e e 2 2 2 2 2 2 e f f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . . f e 4 1 f d d f 1 4 e f . . 
+        . . e f e 4 d d d d 4 e f . . . 
+        . . e 4 d d e 2 2 2 2 f e f . . 
+        . . . e d d e 2 2 2 2 f 4 e . . 
+        . . . . e e f 5 5 4 4 f . . . . 
+        . . . . . f f f f f f f . . . . 
+        . . . . . . . . . f f f . . . . 
+        `],
+    200,
+    characterAnimations.rule(Predicate.MovingDown, Predicate.FacingDown)
+    )
+    if (lighting == 1) {
+        flashlightangle = 1
+        flashlight.direction = 90
+    }
+})
+let lighting = 0
+let flashlightangle = 0
 let flashlight: lightsource.FlashlightLightSource = null
 let mySprite: Sprite = null
 let otherSprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . 1 1 1 1 1 . . 1 1 1 1 1 . . 
-    . . 1 1 1 1 1 . . 1 1 1 1 1 . . 
-    . . 1 1 1 1 1 . . 1 1 1 1 1 . . 
-    . . 1 1 1 1 1 . . 1 1 1 1 1 . . 
-    . . 1 1 1 1 1 . . 1 1 1 1 1 . . 
+    . . . 1 1 1 . . . . 1 1 1 . . . 
+    . . 1 5 f 5 1 . . 1 5 f 5 1 . . 
+    . . 1 5 f 5 1 . . 1 5 f 5 1 . . 
+    . . 1 5 f 5 1 . . 1 5 f 5 1 . . 
+    . . . 1 1 1 . . . . 1 1 1 . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -288,7 +558,7 @@ let otherSprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.npc)
-story.spriteSayText(otherSprite, "test")
+story.spriteSayText(otherSprite, "...")
 sprites.destroy(otherSprite)
 tiles.setCurrentTilemap(tilemap`level2`)
 mySprite = sprites.create(img`
@@ -320,6 +590,7 @@ mySprite,
 60
 )
 flashlight = multilights.flashlightSourceAttachedTo(mySprite)
+flashlightangle = 0
 game.onUpdateInterval(5000, function () {
 	
 })
